40e4130b0e20150afe49d42e21ef879c3318ecbd
Roger Chen <mengqiu.chen@hotmail.com>
2022-06-16 11:39:30 -0600

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,39 @@
 import TaskList from "./Components/TaskList/TaskList";
 import TaskForm from "./Components/TaskForm/TaskForm";
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const [tasks, setTasks] = useState([
-    {task: 'Do exercise', id: '1'},
-    {task: 'Finish the course', id: '2'}
+    { task: "Do exercise", user: "user1", priority: "High", id: "1" },
+    { task: "Finish the course", user: "user2", priority: "", id: "2" },
   ]);
 
-  const deleteTaskHandler = taskId => {
-    setTasks(prevTasks => {
-      const updatedTasks = prevTasks.filter(t => t.id !== taskId);
+  const deleteTaskHandler = (taskId) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.filter((t) => t.id !== taskId);
       return updatedTasks;
-    })
-  }
+    });
+  };
 
-  const addTaskHandler = task => {
-    setTasks(prevTasks => {
+  const addTaskHandler = (task) => {
+    setTasks((prevTasks) => {
       const updatedTasks = [...prevTasks];
-      updatedTasks.unshift({task: task, id: Math.random().toString()});
+      updatedTasks.unshift({ task: task, id: Math.random().toString() });
       return updatedTasks;
-    })
+    });
+  };
+
+  let content = <p style={{ textAlign: "center" }}>No task found</p>;
+
+  if (tasks.length > 0) {
+    content = <TaskList tasks={tasks} onDeleteTask={deleteTaskHandler} />;
   }
-
-  let content = (
-    <p style={{textAlign: 'center'}}>No task found</p>
-  );
-  
-
-  if(tasks.length > 0) {
-    content = (
-      <TaskList tasks={tasks} onDeleteTask={deleteTaskHandler} />
-    )
-  }
-
 
   return (
     <div>
-      <TaskForm onAddTask={addTaskHandler}/>
-      <div>
-        {content}
-      </div>
+      <TaskForm onAddTask={addTaskHandler} />
+      <div>{content}</div>
     </div>
   );
 }
