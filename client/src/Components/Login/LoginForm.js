11eb3a635576e5cccbb3d633feb027cc6d853098
Roger Chen <mengqiu.chen@hotmail.com>
2022-06-23 23:26:38 -0600

    rebounce

--- client/src/Components/Login/LoginForm.js
+++ client/src/Components/Login/LoginForm.js
@@ -11,9 +11,17 @@ const LoginForm = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("check");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 1000);
+
+    return () => {
+      console.log("Clean up");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
