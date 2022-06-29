70a52f9d6958a7e30467c45958b7e7c084f6eddd
Roger Chen <mengqiu.chen@hotmail.com>
2022-06-28 23:02:29 -0600

    update

--- client/src/Components/UI/Banner/Banner.js
+++ client/src/Components/UI/Banner/Banner.js
@@ -1,9 +1,15 @@
 import classes from "./Banner.module.css";
 
 const Banner = (props) => {
-  return <div className={`${classes.banner} ${props.className}`} id={props.id} onClick={props.onClick}>
-    {props.children}
-  </div>
+  return (
+    <div
+      className={`${classes.banner} ${props.className}`}
+      id={props.id}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Banner;
