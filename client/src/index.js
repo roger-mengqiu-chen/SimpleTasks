7d9e89ea349a1619cba9aa4a258e3edd1c0ba4b1
Roger Chen <mengqiu.chen@hotmail.com>
2022-08-30 11:58:01 -0600

    add router

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {AuthContextProvider} from './context/auth-context';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>    
   </AuthContextProvider>
 );
 
